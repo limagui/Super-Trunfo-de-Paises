4c196b53d796a68ed81fca3f1589d9df64d37f8d
João Victor <130244950+limagui@users.noreply.github.com>
2026-05-26 14:10:51 -0300

    inicio do código

---
+++ estrutura/body.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    char codigo[5];
+    char estado;
+    char cidade[20];
+    float area;
+    float pib;
+    int populacao, turistas;
+
+    //Mensagem de boas vinda ao jogo
+    printf("Sem bem-vindo(a) ao jogo Super Trunfo de Países");
+
+    //Inico do código
+
+    return 0;
+
+}
